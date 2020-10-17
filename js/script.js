1d7e1787171ab641d8b22c579b8540a44896de47
Harshit Sharma <iharshitsharma@outlook.com>
2020-10-17 19:45:10 +0530

    done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+function song(){
 fetch("https://rapidapi.p.rapidapi.com/lyrics/zenith%20denzel%20curry", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
-		"x-rapidapi-key": "a699de63cdmsh0ee20836b2cf69cp1d65adjsn0f8393163ea8"
-	}
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
+        "x-rapidapi-key": "a699de63cdmsh0ee20836b2cf69cp1d65adjsn0f8393163ea8"
+    }
 })
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
